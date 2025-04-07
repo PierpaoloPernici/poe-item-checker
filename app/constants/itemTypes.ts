ef332fdd20ee102d86e0e529c75418e560720410
sanzodown <sanzodown@hotmail.fr>
2025-04-07 16:48:25 +0200

    add spears support

--- app/constants/itemTypes.ts
+++ app/constants/itemTypes.ts
@@ -14,6 +14,7 @@ export const ITEM_CLASS_MAP: Record<string, string> = {
   'Daggers': 'weapon.dagger',
   'Rune Daggers': 'weapon.runedagger',
   'Warstaves': 'weapon.warstaff',
+  'Spears': 'weapon.spear',
   "Quarterstaves": 'weapon.warstaff',
   'Body Armours': 'armour.chest',
   'Boots': 'armour.boots',
